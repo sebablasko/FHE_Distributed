d68f7db3c30d81a41084ba78d4506da9f20223c4
Sebastian <sebablasko@gmail.com>
2015-04-10 11:22:39 -0300

    updated run script

--- localTest/run.sh
+++ localTest/run.sh
@@ -7,20 +7,20 @@ echo
 
 echo "Begin Parameter Generation..."
 time ./paramsGenerator.run
-echo "Parameter Generation Done ("$DIFF")"
+echo "Parameter Generation Done"
 echo
 
 echo "Begin Encryption Process..."
 time ./dataEncryptor.run
-echo "Encryption Process Done ("$DIFF")"
+echo "Encryption Process Done"
 echo
 
 echo "Begin Homomorphic Processing..."
 time ./dataProcessor.run
-echo "Homomorphic Processing Done ("$DIFF")"
+echo "Homomorphic Processing Done"
 echo
 
 echo "Begin Decryption Processing..."
 time ./dataDecryptor.run
-echo "Decryption Processing Done ("$DIFF")"
+echo "Decryption Processing Done"
 echo
