27981ea1163c85044b92a17cc6717adedec2ea69
Sebastian <sebablasko@gmail.com>
2015-05-04 13:58:34 -0300

    now the params data is passed with long options

--- localTest/dataProcessor.cpp
+++ localTest/dataProcessor.cpp
@@ -2,34 +2,105 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <getopt.h>
 #include "Processor.h"
 
 using namespace std;
 
+static int verbose_flag;
+
 int main(int argc, char **argv)
 {
-  int mnc, lac, cid, antenna_index;
+  int mnc = -1, lac = -1, cid = -1, antenna_index;
   Processor processor;
-
-  if(argc == 4)
+  
+  while(1)
   {
-    mnc = atoi(argv[1]);
-    lac = atoi(argv[2]);
-    cid = atoi(argv[3]);
+    static struct option long_options[] = 
+    {
+      /* These options set a flag. */
+      {"verbose", no_argument,       &verbose_flag, 1},
+      /* These options don’t set a flag. We distinguish them by their indices. */
+      {"mnc",  required_argument, 0, 'm'},
+      {"lac",  required_argument, 0, 'l'},
+      {"cid",    required_argument, 0, 'c'},
+      /* The close for Options */
+      {0, 0, 0, 0}
+    };
+    
+    /* getopt_long stores the option index here. */
+    int option_index = 0;
+    int opt;
+    
+    opt = getopt_long (argc, argv, "m:l:c:", long_options, &option_index);
+    
+    /* Detect the end of the options. */
+    if (opt == -1)
+      break;
+    
+    switch(opt)
+    {
+      
+      case 0:
+        /* If this option set a flag, do nothing else now. */
+        if (long_options[option_index].flag != 0)
+          break;
+        printf ("option %s", long_options[option_index].name);
+        if (optarg)
+          printf (" with arg %s", optarg);
+        printf ("\n");
+        break;    
+      
+      case 'm':
+        mnc = strtod(optarg, NULL);
+        printf ("option --mnc with value %d\n", mnc);
+        break;
+    
+      case 'l':
+        lac = strtod(optarg, NULL);
+        printf ("option --lac with value %d\n", lac);
+        break;
+    
+      case 'c':
+        cid = strtod(optarg, NULL);
+        printf ("option --cid with value %d\n", cid);
+        break;
+    
+      case '?':
+        /* getopt_long already printed an error message. */
+        break;
+    
+      default:
+        abort ();
+    }
+    
   }
-  else
+  /* Instead of reporting ‘--verbose’
+   and ‘--brief’ as they are encountered,
+   we report the final status resulting from them. */
+  if (verbose_flag)
+    puts("verbose flag is set");
+
+  /* Print any remaining command line arguments (not options). */
+  if (optind < argc)
   {
-    std::cout << "Error, use ./dataProcessor mnc lac cid" << std::endl;
+    printf ("non-option ARGV-elements: ");
+    while (optind < argc)
+      printf("%s ", argv[optind++]);
+    putchar('\n');
+  }
+    
+    
+  /* Check for all parameters */  
+  if(mnc == -1 || lac == -1 || cid == -1)
+  {
+    printf("Error, must explicit mnc, lac and cid code\n");
     return 1;
   }
-
-  std::cout << "Starting processing data: " << std::endl;
-  std::cout << "\tmnc: \t" << mnc << std::endl;
-  std::cout << "\tlac: \t" << lac << std::endl;
-  std::cout << "\tcid: \t" << cid << std::endl;
-
+  
+  /* Everything Ok, start the processing */
   processor.Process(mnc, lac, cid, antenna_index);
   std::cout << "antenna_index value: " << antenna_index << std::endl;
-
+  
   return 0;
 }
